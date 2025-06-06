5091dbe556680e337491483ac503de12be3bb0b2
Espen Moe-Nilssen <espen@medialog.no>
2025-06-06 11:18:13 +0200

    updates

--- src/medialog/docenttheme/viewlets/header_viewlet.py
+++ src/medialog/docenttheme/viewlets/header_viewlet.py
@@ -1,15 +1,20 @@
 # -*- coding: utf-8 -*-
 
 from plone.app.layout.viewlets import ViewletBase
+from plone import api
 
 
 class HeaderViewlet(ViewletBase):
 
     def update(self):
         self.message = self.get_message()
+        self.color = self.get_color()
 
     def get_message(self):
-        return u'My message'
-
+        return api.portal.get_registry_record('DocentIMS.ActionItems.interfaces.IDocentimsSettings.project_title', None)
+                    
+    def get_color(self):
+        return api.portal.get_registry_record('DocentIMS.ActionItems.interfaces.IDocentimsSettings.color1', None) 
+                   
     def index(self):
         return super(HeaderViewlet, self).render()
